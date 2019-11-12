5c6b60e983dba66235547a24f08173a52baddad8
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-12 13:24:31 +0530

    flip coin by loop and dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,27 @@
 #!/bin/bash -x
+headCount=0
+tailCount=0
 randomNum=0
- randomNum=$(( RANDOM%2 ))
- if [ $randomNum -eq 1 ]
- then
-   	echo "head"
+count=0
+declare -A coinCount
+read -p "how many number will the coin be flipped" flips
+while [ $count -le $flips ]	
+do
+	randomNum=$(( RANDOM%2 ))
+ 	if [ $randomNum -eq 1 ]
+ 		then
+   		(( headCount++ ))
 
- else
-	echo "tails"
-fi
+	else
+			(( tailCount++ ))
+	fi
+   		(( count++ ))
+done
+coinCount[head]=$headCount
+coinCount[tail]=$tailCount
+echo "head and tails are " ${sounds[@]}
+headPerCount=$(( ($headCount*100)/$count ))
+tailPerCount=$(( ($tailCount*100)/$count ))
 
 
  
