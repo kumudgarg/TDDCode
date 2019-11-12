604c6e801c2873aa083bbfd6b0c9040cfc4b7148
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-12 18:40:24 +0530

    triplet flip coin function done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,11 +5,18 @@ HH=0
 TT=0
 TH=0
 HT=0
+TTT=0
+HHH=0
+HHT=0
+TTH=0
+HTT=0
+THH=0
+THT=0
+HTH=0
 headCount=0
 tailCount=0
 randomNum=0
 count=0
-count1=0
 declare -A coinCount
 read -p "how many number will the coin be flipped" flips
 function singletCoinComb()
@@ -39,10 +46,11 @@ function flip()
 {
 	coin1=$1
 	coin2=$2
+	coin3=$3
 }
 function doubletCoinComb()
 {
-	while [ $count1 -lt $flips ]
+	while [ $count -lt $flips ]
 	do
 		flip $((RANDOM%2)) $((RANDOM%2)) 
 		if [ $coin1 -eq 0 ]  && [ $coin2 -eq 0 ]
@@ -57,7 +65,7 @@ function doubletCoinComb()
 		else
 			(( TT++ ))
 		fi
-		(( count1++ ))
+		(( count++ ))
 	done
 }
 function PercentageCoin()
@@ -67,13 +75,65 @@ function PercentageCoin()
         THPercount=$(( ($TH*100)/$1 ))
         TTPercount=$(( ($TT*100)/$1 ))
 }
+function tripletCoinComb()
+{
+        while [ $count -lt $flips ]
+        do
+                flip $((RANDOM%2)) $((RANDOM%2)) $((RANDOM%2)) 
+                if [ $coin1 -eq 0 ]  && [ $coin2 -eq 0 ] && [ $coin3 -eq 0 ]
+                then
+                        (( HHH++ ))
+                elif [ $coin1 -eq 0 ] && [ $coin2 -eq 1 ] && [ $coin3 -eq 0 ]
+                then
+                        (( HTH++ ))
+                elif [ $coin1 -eq 1 ] && [ $coin2 -eq 0 ] && [ $coin3 -eq 1 ]
+                then
+                        (( THT++ ))
+		elif [ $coin1 -eq 1 ] && [ $coin2 -eq 1 ] && [ $coin3 -eq 0 ]
+                then
+                        (( TTH++ ))
+		elif [ $coin1 -eq 0 ] && [ $coin2 -eq 1 ] && [ $coin3 -eq 1 ]
+                then
+                        (( HTT++ ))
+		elif [ $coin1 -eq 1 ] && [ $coin2 -eq 0 ] && [ $coin3 -eq 0 ]
+                then
+                        (( THH++ ))
+		elif [ $coin1 -eq 0 ] && [ $coin2 -eq 0 ] && [ $coin3 -eq 1 ]
+                then
+                        (( HHT++ ))
+		
+                else
+                        (( TTT++ ))
+                fi
+	  	(( count++ ))
+        done
+}
+
+		
+function tripletPercentageCoin()
+{
+        HHHPercount=$(( ($HHH*100)/$1 ))
+        HTHPercount=$(( ($HTH*100)/$1 ))
+        THHPercount=$(( ($THH*100)/$1 ))
+        TTHPercount=$(( ($TTH*100)/$1 ))
+	THTPercount=$(( ($THT*100)/$1 ))
+	HTTPercount=$(( ($HTT*100)/$1 ))
+	HHTPercount=$(( ($HHT*100)/$1 ))
+	TTTPercount=$(( ($TTT*100)/$1 ))
+
+}
+
 for(( i=0; i <= 1; i++ ))
         do
         {
                 #singletCoinComb
                 #singletInfo $(( $count ))
-                doubletCoinComb
-                PercentageCoin $(( $count1 ))
+                #doubletCoinComb
+               	#PercentageCoin $(( $count ))
+		tripletCoinComb
+		tripletPercentageCoin $(( $count ))
+		
         }
 
 done
+
