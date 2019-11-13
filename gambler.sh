082a7028ac111114d2583326acc70a2a931518ad
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-13 14:50:24 +0530

    win and loose in gambling

--- gambler.sh
+++ gambler.sh
@@ -2,6 +2,8 @@
 bets=0
 stake=0
 bets=0
+win=0
+loose=0
 read -p "Insert days for playing" day
 read -p "number of game" game
 for (( i=0; i < $day; i++ ))
@@ -9,11 +11,17 @@ do
 	(( stake=$(( stake+100 )) ))
 	for (( j=0; j < $game; j++ ))
 	do
+		randNum=$(( RANDOM%2 ))
 		 (( bets++ ))
 		if [ $stake -eq 0 ]
 		then
 			break
+		elif [ $randNum -eq 1 ]
+		then
+			(( win++ ))
+			(( stake++ ))
 		else
+			(( loose++ ))
 			(( stake-- ))	
 		fi
 		
@@ -21,3 +29,5 @@ do
 done
 echo "total bets are $bets " 
 echo "total stake is $stake "
+echo "total win times $win "
+echo "total  loose times $loose "
