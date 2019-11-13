6b357cbbe672cd05245537e2b058535e562bed98
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-13 17:57:32 +0530

    50% of stack then resign that day updated

--- gambler.sh
+++ gambler.sh
@@ -4,16 +4,21 @@ stake=0
 bets=0
 win=0
 loose=0
+k=0
 read -p "Insert days for playing" day
 read -p "number of game" game
 read -p "Insert win/loose percent" stakepercent
 for (( i=0; i < $day; i++ ))
 do
 	(( stake=$(( stake+100 )) ))
+	stackAmountWin=$(( stakepercent + 100 ))
+        stackAmountLoose=$(( 100 - stakepercent ))
+        winLimit=$(( ($stake * $stackAmountWin) / 100 ))
+        LooseLimit=$(( ($stake * $stackAmountLoose) / 100 ))
 	for (( j=0; j < $game; j++ ))
 	do
 		randNum=$(( RANDOM%2 ))
-		 (( bets++ ))
+		(( bets++ ))
 		if [ $stake -eq 0 ]
 		then
 			break
@@ -25,20 +30,17 @@ do
 			(( loose++ ))
 			(( stake-- ))	
 		fi
-		stackAmountWin=$(( stackpercent + 100 ))
-		stackAmountLoose=$(( 100 - stackpercent ))
-		winLimit=$(( ($stake * $stackAmountWin) / 100 ))
-		LooseLimit=$(( ($stake * $stackAmountLoose) / 100 ))
-		if [ $win -eq $winLimit ]
-        	then 
-                	break
-		elif [ $loose -eq $LooseLimit ]
-		then
-			break
-		else
-                	continue
-        	fi
+		if [ $win -ge $winLimit ]
+	        then 
+	                break
+	        elif [ $LooseLimit -le $loose ]
+	        then
+	                break
+	        else
+	                echo "continue gambler's day"
+	        fi
 
+	
 	done
 done
 echo "total bets are $bets " 
